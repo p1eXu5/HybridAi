91698f6b42f7674b2008bfcb0476f2a19e009822
p1eXu5 <v1adp1exu5@gmail.com>
2019-11-28 23:48:08 +0300

    chore: add .gitignore file, source projects and tests solution folder

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
