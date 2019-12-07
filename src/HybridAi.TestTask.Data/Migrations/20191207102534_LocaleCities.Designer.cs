e15c94eba2dc7e2e3a065d52ba31fa36da3b79fb
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-07 13:41:35 +0300

    chore: add LocaleCities migration

--- src/HybridAi.TestTask.Data/Migrations/20191207102534_LocaleCities.Designer.cs
+++ src/HybridAi.TestTask.Data/Migrations/20191207102534_LocaleCities.Designer.cs
@@ -2,15 +2,17 @@
 using HybridAi.TestTask.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HybridAi.TestTask.Data.Migrations
 {
     [DbContext(typeof(IpDbContext))]
-    partial class IpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191207102534_LocaleCities")]
+    partial class LocaleCities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
