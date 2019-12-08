335d0030340450338841115d4366651aad50fbee
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 11:05:53 +0300

    tests: add Downloader Unit Tests and ensure:     Process__NotUrlRequest_SuccessorIsNull__ReturnsResponseWithSameRequest     Process__UrlIsNotExisted_SuccessorIsNull__ReturnsUrlResponse

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/UnitTests/ChainLinks/DownloaderTests.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Unit tests for Downloader class.
+ * ================================
+ *
+ */
+
+using HybridAi.TestTask.ConsoleDbUpdater.ChainLinks;
+using HybridAi.TestTask.ConsoleDbUpdater.Models;
+using NUnit.Framework;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.UnitTests.ChainLinks
+{
+    [TestFixture]
+    public class DownloaderTests
+    {
+        [Test]
+        public void Process__NotUrlRequest_SuccessorIsNull__ReturnsResponseWithSameRequest()
+        {
+            // Arrange:
+            var chain = _getDownloader();
+            var request = Request.EmptyRequest;
+
+            // Action:
+            var response = chain.Process( request );
+
+            // Assert:
+            Assert.IsTrue( ReferenceEquals( response.Request, request ) );
+            Assert.IsFalse( response is Response< FileLocationRequest > );
+        }
+
+        [Test]
+        public void Process__UrlIsNotExisted_SuccessorIsNull__ReturnsUrlResponse()
+        {
+            // Arrange:
+            var chain = _getDownloader();
+
+            // Action:
+            var response = chain.Process( new UrlRequest( "https://a.a/a.zip" ) );
+
+            // Assert:
+            Assert.IsTrue( response is Response< UrlRequest > );
+        }
+
+        #region factory
+        // Insert factory methods here:
+        private Downloader _getDownloader()
+        {
+            return new Downloader( null );
+        }
+
+        #endregion
+    }
+}
