2984849cd8934c0e24c3e860f3a1087d55618557
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 14:02:33 +0300

    chore: add Process logic into Unzipper

--- src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Unzipper.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Unzipper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,65 @@ using HybridAi.TestTask.ConsoleDbUpdater.Models;
 namespace HybridAi.TestTask.ConsoleDbUpdater.ChainLinks
 {
     public class Unzipper : ChainLink
-
     {
+        public const string ZIP_EXT = ".zip";
+        public readonly string[] MaintainedExtensions = new[] { ".txt", ".csv" };
+
         public Unzipper( ChainLink? successor ) 
             : base( successor )
         { }
 
         public override IResponse< Request > Process( Request request )
         {
+            if ( request is FileLocationRequest fileLocationRequest ) 
+            {
+                string path = fileLocationRequest.Path;
+                if ( !Path.GetExtension( path ).Equals( ZIP_EXT, StringComparison.InvariantCultureIgnoreCase ) ) {
+                    return base.Process( request );
+                }
+
+                try {
+                    using ZipArchive zipArchive = ZipFile.OpenRead( fileLocationRequest.Path );
+                    
+                    if ( zipArchive.Entries.Count == 0 ) {
+                        return base.Process( request );
+                    }
+
+                    // check directory
+                    string destinationPath = Path.Combine( Path.GetTempPath(), Path.GetFileNameWithoutExtension( path ) );
+                    destinationPath += Path.DirectorySeparatorChar;
+                        
+                    if ( !Directory.Exists( destinationPath ) ) {
+                        Directory.CreateDirectory( destinationPath );
+                    }
+
+                    List< string > targets = new List< string >( zipArchive.Entries.Count );
+
+                    foreach ( ZipArchiveEntry entry in zipArchive.Entries ) 
+                    {
+                        if ( MaintainedExtensions.Contains( Path.GetExtension( entry.FullName ) ) ) {
+                            var extractPath = Path.Combine( destinationPath, Path.GetFileNameWithoutExtension( entry.FullName ) );
+
+                            try {
+                                entry.ExtractToFile( extractPath, true );
+                            }
+                            catch ( Exception ex ) {
+                                LoggerFactory.Instance.Log( ex.Message );
+                            }
+
+                            targets.Add( extractPath );
+                        }
+                    }
+
+                    if ( targets.Count > 0 ) {
+                        return base.Process( new FolderRequest( targets ) );
+                    }
+                }
+                catch ( Exception ex ) {
+                    LoggerFactory.Instance.Log( ex.Message );
+                }
+            }
+
             return base.Process( request );
         }
     }
