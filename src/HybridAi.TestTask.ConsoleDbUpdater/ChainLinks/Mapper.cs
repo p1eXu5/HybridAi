ca21c7214b728473b4a65962281ef8ef12d050d8
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 14:45:42 +0300

    chore: add overriding Process to Mapper

--- src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Mapper.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Mapper.cs
@@ -9,8 +9,13 @@ namespace HybridAi.TestTask.ConsoleDbUpdater.ChainLinks
 {
     public class Mapper : ChainLink
     {
-        public Mapper( ChainLink successor ) 
+        public Mapper( ChainLink? successor ) 
             : base( successor )
         { }
+
+        public override IResponse< Request > Process( Request request )
+        {
+            return base.Process( request );
+        }
     }
 }
