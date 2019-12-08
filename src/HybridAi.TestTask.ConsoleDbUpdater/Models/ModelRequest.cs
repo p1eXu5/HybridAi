dbdeeb3ffd3b924c6f785868e6871edc68c48241
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 14:46:02 +0300

    chore: add ModelRequest<T>

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Models/ModelRequest.cs
@@ -0,0 +1,12 @@
+﻿using HybridAi.TestTask.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Models
+{
+    public class ModelRequest< TModel > : Request
+        where TModel : IEntity
+    {
+    }
+}
