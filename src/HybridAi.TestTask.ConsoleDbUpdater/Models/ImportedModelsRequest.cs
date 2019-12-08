41706ef6afef150374f6ad3f8fd33a36e9bb1785
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 22:08:18 +0300

    chore: add ImportedModelsRequest

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Models/ImportedModelsRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HybridAi.TestTask.Data.Models;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Models
+{
+    public class ImportedModelsRequest : Request
+    {
+        public ImportedModelsRequest( ICollection< IEntity >[] modelCollections )
+        {
+            ModelCollections = modelCollections;
+        }
+
+        public ICollection< IEntity >[] ModelCollections { get; }
+    }
+}
