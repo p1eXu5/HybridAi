9d335621c98ee9886904a11d720920b43d7c9db4
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 14:01:26 +0300

    chore: add simple logger

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/LoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater
+{
+    public class LoggerFactory
+    {
+        private static LoggerFactory _instance;
+
+        public static LoggerFactory Instance
+            => _instance ??= new LoggerFactory();
+
+        protected LoggerFactory() { }
+
+        private ILogger? _logger;
+
+        public ILogger Logger
+        {
+            get { return _logger ??= new ConsoleLogger(); }
+            set => _logger = value;
+        }
+
+        public void Log( string msg )
+        {
+            Logger.WriteLine( msg );
+        }
+    }
+}
