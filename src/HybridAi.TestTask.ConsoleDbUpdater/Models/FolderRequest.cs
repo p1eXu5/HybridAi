152ae67cc0c07ad0d788da5cf3038e68864cd672
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 12:01:31 +0300

    chore: add FolderRequest model

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Models/FolderRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Models
+{
+    public class FolderRequest : Request
+    {
+        public FolderRequest( IEnumerable< FileInfo > fileInfos )
+        {
+            FileInfos = fileInfos.ToArray();
+        }
+
+
+        public FileInfo[] FileInfos { get; }
+
+        public override IResponse< Request > Response => new Response< FolderRequest >( this );
+    }
+}
