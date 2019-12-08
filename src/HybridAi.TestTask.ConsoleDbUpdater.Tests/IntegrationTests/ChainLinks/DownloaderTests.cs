fa04a6da771f84ac466abb8b2639eb654502540d
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-08 11:06:34 +0300

    test: ensure that Downloader's     Process__DEFAULT_FILE_URL__DownloadsFileIntoInternetCacheFolder

--- src/HybridAi.TestTask.ConsoleDbUpdater.Tests/IntegrationTests/ChainLinks/DownloaderTests.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/IntegrationTests/ChainLinks/DownloaderTests.cs
@@ -1,9 +1,19 @@
-﻿using NUnit.Framework;
+﻿/*
+ * Integration tests for Downloader class.
+ * =======================================
+ *
+ */
+
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using HybridAi.TestTask.ConsoleDbUpdater.ChainLinks;
+using HybridAi.TestTask.ConsoleDbUpdater.Models;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.IntegrationTests.ChainLinks
 {
@@ -12,13 +22,26 @@ namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.IntegrationTests.ChainLinks
 	public class DownloaderTests
 	{
 		[Test]
-		public void Process_UrlArgument_ReturnsUrlResponse()
-		{
-			
-		}
+		public void Process__DEFAULT_FILE_URL__DownloadsFileIntoInternetCacheFolder()
+        {
+			// Arrange:
+            var downloader = _getDownloader();
+            const string url = Program.DEFAULT_FILE_URL;
+            var urlRequest = new UrlRequest( url );
+
+            // Action:
+            var response = (IResponse< FileLocationRequest >)downloader.Process( urlRequest );
+
+            // Assert:
+            Assert.IsTrue( File.Exists( response.Request.Path ) );
+        }
 
 		#region factory
 		// Insert factory methods here:
+        private Downloader _getDownloader()
+        {
+			return new Downloader( null );
+        }
 
 		#endregion
 	}
