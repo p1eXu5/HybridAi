2649708fe683d5649fc7209f24dcee64021cf719
Vladimir <lihatskiy@optimizon.ru>
2019-12-09 12:39:06 +0300

    test: add Updater integration tests class

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/IntegrationTests/ChainLinks/UpdaterTests.cs
@@ -0,0 +1,32 @@
+﻿using HybridAi.TestTask.ConsoleDbUpdater.ChainLinks;
+using HybridAi.TestTask.ConsoleDbUpdater.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.IntegrationTests.ChainLinks
+{
+    public class UpdaterTests
+    {
+        [Test]
+        public void ctor__ImportedModelsRequestNotEmpty_SuccessorIsNull__CanCreateDatabase()
+        {
+
+        }
+
+
+        #region factory
+		// Insert factory methods here:
+
+        private Updater _getMapper()
+        {
+            return new Updater( null );
+        }
+
+
+		#endregion
+    }
+}
