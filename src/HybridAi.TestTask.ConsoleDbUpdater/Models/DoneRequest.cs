df50c8c944146538328b0c2ab67cdbc7508e6a19
Vladimir <lihatskiy@optimizon.ru>
2019-12-09 12:37:32 +0300

    chore: add DoneRequest

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Models/DoneRequest.cs
@@ -0,0 +1,19 @@
+﻿using HybridAi.TestTask.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Models
+{
+    public class DoneRequest : Request
+    {
+        public DoneRequest( string message )
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+
+        public override IResponse<Request> Response => new Response< DoneRequest >( this );
+    }
+}
