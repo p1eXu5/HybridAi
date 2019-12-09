9129d00dfacefb4d96f948f87f86c824a7234d25
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-09 20:09:29 +0300

    clean: DbContextOptionsFactoryTests

--- src/HybridAi.TestTask.DataTests/IntegrationTests/DbContextOptionsFactoryTests.cs
+++ src/HybridAi.TestTask.DataTests/IntegrationTests/DbContextOptionsFactoryTests.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using HybridAi.TestTask.Data;
+﻿using HybridAi.TestTask.Data;
 using NUnit.Framework;
 
 namespace HybridAi.TestTask.DataTests.IntegrationTests
