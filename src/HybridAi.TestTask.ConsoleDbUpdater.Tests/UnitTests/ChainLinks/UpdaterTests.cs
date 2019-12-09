963b712d4042c27055e2846ee424d19eab1c46f0
Vladimir <lihatskiy@optimizon.ru>
2019-12-09 12:38:10 +0300

    tests: ensure that Updater's     Process__NotImportedModelsRequest_SuccessorIsNull__ReturnsResponseWithSameRequest     ctor__ImportedModelsRequestIsEmpty_SuccessorIsNull__ReturnsResponseWithDoneRequest     ctor__ImportedModelsRequestIsEmpty_SuccessorIsNull__ReturnsResponseWithRequestWithExpectedMessage

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/UnitTests/ChainLinks/UpdaterTests.cs
@@ -0,0 +1,77 @@
+﻿using HybridAi.TestTask.ConsoleDbUpdater.ChainLinks;
+using HybridAi.TestTask.ConsoleDbUpdater.Models;
+using HybridAi.TestTask.Data.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.UnitTests.ChainLinks
+{
+    public class UpdaterTests
+    {
+        [Test]
+		public void Process__NotImportedModelsRequest_SuccessorIsNull__ReturnsResponseWithSameRequest()
+		{
+            // Arrange:
+            Updater mapper = _getMapper();
+            var request = Request.EmptyRequest;
+
+            // Action:
+            var response = mapper.Process( request );
+
+            // Assert:
+            Assert.IsTrue( ReferenceEquals( response.Request, request ) );
+		}
+
+        [Test]
+        public void ctor__ImportedModelsRequestIsEmpty_SuccessorIsNull__ReturnsResponseWithDoneRequest()
+        {
+            // Arrange:
+            Updater mapper = _getMapper();
+            var request = _getEmptyImportedModelsRequest();
+
+            // Action:
+            var response = mapper.Process( request );
+
+            // Assert:
+            Assert.IsFalse( ReferenceEquals( response.Request, request ) );
+            Assert.IsTrue( response is Response< DoneRequest > );
+        }
+
+        [Test]
+        public void ctor__ImportedModelsRequestIsEmpty_SuccessorIsNull__ReturnsResponseWithRequestWithExpectedMessage()
+        {
+            // Arrange:
+            Updater mapper = _getMapper();
+            var request = _getEmptyImportedModelsRequest();
+
+            // Action:
+            var message = ((IResponse< DoneRequest >) mapper.Process( request )).Request.Message;
+
+            // Assert:
+            StringAssert.Contains( "Imported model collections are empty.", message );
+        }
+
+
+        #region factory
+		// Insert factory methods here:
+
+        private Updater _getMapper()
+        {
+            return new Updater( null );
+        }
+
+        private ImportedModelsRequest _getEmptyImportedModelsRequest()
+        {
+            List< IEntity > model = new List< IEntity >() { };
+            List< IEntity >[] models = new List< IEntity >[] { model };
+
+            return new ImportedModelsRequest( models );
+        }
+
+		#endregion
+    }
+}
--- src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Updater.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater/ChainLinks/Updater.cs
@@ -9,8 +9,20 @@ namespace HybridAi.TestTask.ConsoleDbUpdater.ChainLinks
 {
     public class Updater : ChainLink
     {
-        public Updater( ChainLink successor ) 
+        public Updater( ChainLink? successor ) 
             : base( successor )
         { }
+
+        public override IResponse<Request> Process( Request request )
+        {
+            if ( request is ImportedModelsRequest modelsRequest ) {
+                if ( !modelsRequest.ModelCollections.Any() 
+                    || modelsRequest.ModelCollections.All( c => !c.Any() ) ) {
+                    return new DoneRequest( "Imported model collections are empty." ).Response;
+                }
+            }
+
+            return base.Process( request );
+        }
     }
 }
