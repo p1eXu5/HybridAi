bd908f000a56182a3e6cef6112ed4be3192bcdde
First Second <v1adp1exu5@gmail.com>
2019-12-11 21:56:26 +0300

    tests: ensure that ChainBuilderExtensions's     CopyCityLocationCity_CityLocationIsNull_CopyCityLocation     CopyCityLocationCity_CityLocationIsNotNull_CityIsNull__CopyCity

--- src/HybridAi.TestTask.ConsoleDbUpdater.Tests/UnitTests/Extensions/ChainBuilderExtensionsTests.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/UnitTests/Extensions/ChainBuilderExtensionsTests.cs
@@ -14,22 +14,47 @@ namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.UnitTests.Extensions
 	public class ChainBuilderExtensionsTests
 	{
 		[Test]
-		public void CopyCity_CityNotNull_DoesNotCopy()
+		public void CopyCityLocationCity_CityLocationIsNull_CopyCityLocation()
 		{
 			// Arrange:
-            var cb = new CityBlock();
+            var cityBlock = new CityBlock();
             var city = new EnCity( 1 ) { CityName = "Test City" };
             var cityLocation = new CityLocation( 1 ) {
                 EnCity = city
             };
 
             // Action:
-			Assert.IsNull( cb.CityLocation );
-			cb.CopyCityLocationCity( cityLocation );
+			Assert.IsNull( cityBlock.CityLocation );
+			cityBlock.CopyCityLocationCity( cityLocation );
 
             // Assert:
-			Assert.IsTrue( ReferenceEquals( cityLocation, cb.CityLocation ) );
-			Assert.IsTrue( ReferenceEquals( city, cb.CityLocation.EnCity ) );
+			Assert.IsTrue( ReferenceEquals( cityLocation, cityBlock.CityLocation ) );
+			Assert.IsTrue( ReferenceEquals( city, cityBlock.CityLocation.EnCity ) );
+        }
+
+        [Test]
+		public void CopyCityLocationCity_CityLocationIsNotNull_CityIsNull__CopyCity()
+		{
+			// Arrange:
+            var containedCity = new EnCity( 1 ) { CityName = "Test City" };
+            var containedCityLocation = new CityLocation( 1 ) {
+                EnCity = containedCity
+            };
+            var cityBlock = new CityBlock {
+                CityLocation = containedCityLocation
+            };
+
+            var newCity = new EsCity( 1 ) { CityName = "EsCity" };
+            var copiedCityLocation = new CityLocation( 1 ) {
+                EsCity = newCity
+            };
+
+            // Action:
+			cityBlock.CopyCityLocationCity( copiedCityLocation );
+
+            // Assert:
+			Assert.IsTrue( ReferenceEquals( newCity, cityBlock.CityLocation.EsCity ) );
+			Assert.IsTrue( ReferenceEquals( containedCity, cityBlock.CityLocation.EnCity ) );
         }
 
 		#region factory
--- src/HybridAi.TestTask.ConsoleDbUpdater/Extensions/CityBlockExtensions.cs
+++ src/HybridAi.TestTask.ConsoleDbUpdater/Extensions/CityBlockExtensions.cs
@@ -1,15 +1,45 @@
 ﻿using HybridAi.TestTask.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace HybridAi.TestTask.ConsoleDbUpdater.Extensions
 {
     public static class CityBlockExtensions
     {
-        public static void CopyCityLocationCity( this CityBlock block, CityLocation city )
+        public static void CopyCityLocationCity( this CityBlock block, CityLocation cityLocation )
         {
+            if ( block.CityLocation == null ) {
+                block.CityLocation = cityLocation;
+            }
+            else {
+                var blockCityLocation = block.CityLocation;
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.EnCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.RuCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.EsCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.DeCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.FrCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.JaCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.PtBrCity ) );
+                _copyCity( blockCityLocation, cityLocation, nameof( blockCityLocation.ZhCnCity ) );
+            }
+        }
 
+        private static void _copyCity( CityLocation clkA, CityLocation clkB, string propertyName )
+        {
+            PropertyInfo? clkAPropertyInfo = clkA.GetType().GetProperty( propertyName, BindingFlags.Instance | BindingFlags.Public );
+            PropertyInfo? clkBPropertyInfo = clkB.GetType().GetProperty( propertyName, BindingFlags.Instance | BindingFlags.Public );
+
+            if ( clkAPropertyInfo != null && clkBPropertyInfo != null ) {
+
+                object? valB = clkBPropertyInfo.GetValue( clkB );
+
+                if ( clkAPropertyInfo.GetValue( clkA ) == null && valB != null ) {
+                    clkAPropertyInfo.SetValue( clkA, valB );
+                }
+
+            }
         }
     }
 }
