fa6a52ca02c04030013d5426b4e8b567375bba81
Vladimir <lihatskiy@optimizon.ru>
2019-12-12 08:34:05 +0300

    test: add TestLoggerExtensions

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/TestHelpers/TestLoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.TestHelpers
+{
+    public static class TestLoggerExtensions
+    {
+        public static string GetMessages( this ILogger logger )
+        {
+            return ((TestLogger)logger).Messages;
+        }
+    }
+}
