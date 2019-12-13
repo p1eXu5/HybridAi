fa39bf4645d2b4b5b4e8f6d43ccee76bc374eeba
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-13 15:10:27 +0300

    test: add ChainBilderTests

---
+++ src/HybridAi.TestTask.ConsoleDbUpdater.Tests/IntegrationTests/ChainBilderTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HybridAi.TestTask.ConsoleDbUpdater.Tests.IntegrationTests
+{
+
+	[TestFixture]
+	public class ChainBilderTests
+	{
+		[Test]
+		public void unit()
+		{
+
+		}
+
+		#region factory
+		// Insert factory methods here:
+
+		#endregion
+	}
+
+}
