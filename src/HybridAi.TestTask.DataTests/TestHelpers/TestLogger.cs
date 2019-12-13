df4f751453ae5ca9698dd41a04ec45b4ad94d3ce
p1eXu5 <v1adp1exu5@gmail.com>
2019-12-13 12:23:56 +0300

    test: ensure that IpDbContext's SaveChanges_AddCityLocationToCityBlock_CanUpdateCity

---
+++ src/HybridAi.TestTask.DataTests/TestHelpers/TestLogger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HybridAi.TestTask.ConsoleDbUpdater;
+
+namespace HybridAi.TestTask.DataTests.TestHelpers
+{
+    internal class TestLogger : ILogger
+    {
+        private readonly List< string > _messages = new List< string >();
+
+        public void WriteLine( string message )
+        {
+            _messages.Add( message );
+        }
+
+        public string Messages
+        {
+            get { return _messages.Aggregate( "\n", ( s1, s2 ) => { return $"{s1}\n{s2}"; } ); }
+        }
+    }
+
+}
---
+++ src/HybridAi.TestTask.DataTests/UnitTests/IpDbContextTests.cs
@@ -0,0 +1,77 @@
+﻿using HybridAi.TestTask.ConsoleDbUpdater;
+using NUnit.Framework;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using HybridAi.TestTask.Data;
+using HybridAi.TestTask.Data.Models;
+using HybridAi.TestTask.DataTests.TestHelpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace HybridAi.TestTask.DataTests.UnitTests
+{
+
+	[TestFixture]
+	public class IpDbContextTests
+	{
+        [ OneTimeSetUp ]
+        public void Setup()
+        {
+            LoggerFactory.Instance.Logger = new TestLogger();
+        }
+
+
+		[Test]
+		public void SaveChanges_AddCityLocationToCityBlock_CanUpdateCity()
+        {
+            // Arrange:
+            var cityBlock = new CityBlockIpv4( "1.1.1.1" );
+
+            using ( var ctx = new IpDbContext( _getOptions() ) ) {
+                ctx.Add( cityBlock );
+                ctx.SaveChanges();
+            }
+
+            // Action:
+            var cityLocation = new CityLocation( 1 ) {
+                EnCity = new EnCity( 1 ),
+                EsCity = new EsCity( 1 ),
+            };
+
+            using ( var ctx = new IpDbContext( _getOptions() ) ) {
+                var dbBlock = ctx.CityBlockIpv4Collection.First();
+                dbBlock.CityLocation = cityLocation;
+                ctx.Add( cityLocation );
+                ctx.Add( cityLocation.EnCity );
+                ctx.Add( cityLocation.EsCity );
+                ctx.SaveChanges();
+            }
+
+            // Assert:
+            using ( var ctx = new IpDbContext( _getOptions() ) ) {
+                var dbBlock = ctx.CityBlockIpv4Collection.Include( c => c.CityLocation )
+                                                         .ThenInclude( c => c.EnCity )
+                                                         .Include( c => c.CityLocation )
+                                                         .ThenInclude( c => c.EsCity )
+                                                         .First();
+                Assert.NotNull( dbBlock.CityLocation );
+                Assert.NotNull( dbBlock.CityLocation.EnCity );
+                Assert.NotNull( dbBlock.CityLocation.EsCity );
+            }
+        }
+
+
+		#region factory
+		// Insert factory methods here:
+
+        private DbContextOptions< IpDbContext > _getOptions( [CallerMemberName]string dbName = null )
+        {
+            var options = new DbContextOptionsBuilder< IpDbContext >().UseInMemoryDatabase( dbName ?? "TestDb" ).Options;
+            DbContextOptionsFactory.Instance.DbContextOptions = options;
+
+            return options;
+        }
+
+		#endregion
+	}
+
+}
